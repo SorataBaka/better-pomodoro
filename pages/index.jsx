29c93592f11405ea43d9ce7fef82bf80a5cc774a
Reynadi <yerpa531@gmail.com>
2022-11-05 21:42:10 +0700

    chore: added href to start here button

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,19 @@
 import { Inter } from "@next/font/google";
+import Link from "next/link";
 
 const App = () => {
-	return (
-		<>
-			<div className="h-screen w-screen flex flex-col justify-center items-center">
-				<h1 className="text-6xl font-bold text-center">A Better Pomodoro</h1>
-				<button className="font-semibold text-lg mt-4 py-2 px-4 bg-slate-300 rounded hover:bg-opacity-75">
-					Start Here
-				</button>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="h-screen w-screen flex flex-col justify-center items-center">
+        <h1 className="text-6xl font-bold text-center">A Better Pomodoro</h1>
+        <Link href="/settings">
+          <button className="font-semibold text-lg mt-4 py-2 px-4 bg-slate-300 rounded hover:bg-opacity-75">
+            Start Here
+          </button>
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default App;
