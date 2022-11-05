4ccabf2962fa8b57a3557ef22b48b51b27346877
Reynadi <yerpa531@gmail.com>
2022-11-05 21:59:59 +0700

    feat: styled timer and index

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,14 +4,16 @@ import Link from "next/link";
 const App = () => {
   return (
     <>
-      <div className="h-screen w-screen flex flex-col justify-center items-center">
-        <h1 className="text-6xl font-bold text-center">A Better Pomodoro</h1>
-        <Link href="/settings">
-          <button className="font-semibold text-lg mt-4 py-2 px-4 bg-slate-300 rounded hover:bg-opacity-75">
-            Start Here
-          </button>
-        </Link>
-      </div>
+      <main className="bg-[#e8ecfa]">
+        <div className="h-screen w-screen flex flex-col justify-center items-center">
+          <h1 className="text-6xl font-bold text-center">A Better Pomodoro</h1>
+          <Link href="/settings">
+            <button className="font-semibold text-lg mt-4 py-2 px-4 bg-[#c7d7fd] rounded-lg hover:bg-opacity-75">
+              Start Here
+            </button>
+          </Link>
+        </div>
+      </main>
     </>
   );
 };
