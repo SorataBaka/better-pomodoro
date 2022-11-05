e919d7ccef52a3e69accdd4450fb67b6e04b98d1
Reynadi <yerpa531@gmail.com>
2022-11-05 19:45:59 +0700

    feat: index done

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,12 @@
+import { Inter } from "@next/font/google";
+
 const App = () => {
   return (
     <>
-      <h1 className="font-bold">ok</h1>
+      <div className="h-screen w-screen flex flex-col justify-center items-center">
+        <h1 className="text-6xl font-bold text-center">A Better Pomodoro</h1>
+        <button className="font-semibold text-lg mt-4 py-2 px-4 bg-slate-300 rounded hover:bg-opacity-75">Start Here</button>
+      </div>
     </>
   );
 };
