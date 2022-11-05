90f1bcc6b948159409968074688803bad40ca9dc
Christian Harjuno <harjuno.tian@gmail.com>
2022-11-06 02:39:34 +0900

    base rule layout

--- pages/settings.jsx
+++ pages/settings.jsx
@@ -11,6 +11,18 @@ export default function Settings() {
 
 	const [sessions, setSession] = useState(0);
 
+	const [rules, setRules] = useState([]);
+
+	const [currentRuleSelect, setCurrentRuleSelect] = useState(1);
+
+	const availableRules = [
+		{ name: "On Even", rule_id: 1 },
+		{ name: "On Odd", rule_id: 2 },
+		{ name: "Every Session", rule_id: 3 },
+		{ name: "On Nth", rule_id: 4 },
+		{ name: "Every Nth", rule_id: 5 },
+	];
+
 	const submit = () => {
 		const newConfig = {
 			is_loop: isLoop,
@@ -20,6 +32,22 @@ export default function Settings() {
 			starting_task: taskHour * 60 + taskMinute,
 		};
 		console.log(newConfig);
+	};
+
+	const addRule = () => {
+		const newArray = [
+			...rules,
+			{
+				name: availableRules[currentRuleSelect - 1].name,
+				rule_id: currentRuleSelect,
+				add: 0,
+				type: "task",
+				has_variable: false,
+				position: 0,
+			},
+		];
+		setRules(newArray);
+		setCurrentRuleSelect(1);
 	};
 
 	return (
@@ -79,7 +107,7 @@ export default function Settings() {
 					</div>
 					<div className={style.sidebarsettingdiv}>
 						<h3>Repeat Forever?</h3>
-						<label class={style.switch}>
+						<label className={style.switch}>
 							<input
 								type="checkbox"
 								checked={isLoop}
@@ -114,6 +142,31 @@ export default function Settings() {
 			<div className={style.rulebar}>
 				<div className={style.rulebarinner}>
 					<h1>Pomodoro Rules</h1>
+					<div className={style.rulediv}>
+						<select
+							name="rules"
+							value={currentRuleSelect}
+							onChange={(e) => {
+								setCurrentRuleSelect(e.target.value);
+							}}
+						>
+							{availableRules.map((rule) => {
+								return (
+									<option value={rule.rule_id} key={rule.rule_id}>
+										{rule.name}
+									</option>
+								);
+							})}
+						</select>
+						<button onClick={addRule}>+</button>
+					</div>
+					{rules.map((rule, index) => {
+						return (
+							<div className={style.addedrulediv} key={index}>
+								{rule.name}
+							</div>
+						);
+					})}
 				</div>
 			</div>
 		</div>
