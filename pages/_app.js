2e214e8d3d9336a6e90169a6e9d4d9351018463b
Christian Harjuno <harjuno.tian@gmail.com>
2022-11-06 12:33:31 +0900

    toaster and router

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
-
+import "../styles/globals.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<ToastContainer draggable={false} />
+			<Component {...pageProps} />
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
